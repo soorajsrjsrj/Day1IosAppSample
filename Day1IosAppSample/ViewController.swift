a6527214533651623dc92bba27c76fc30517fb9e
Ilyas <Ilyasderaiya@gmail.com>
2019-03-04 18:46:28 -0500

    Initial Commit

---
+++ Day1IosAppSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Day1IosAppSample
+//
+//  Created by SOORAJ SUNEESH on 2019-03-04.
+//  Copyright © 2019 SOORAJ SUNEESH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
